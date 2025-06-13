0a64deae6828f5e1a46c406a58cfcbe7c52508e9
Marcos Madeira <mmadeirasilva5@gmail.com>
2025-06-13 18:43:55 -0300

    adapter for ec2

--- app/core/config.py
+++ app/core/config.py
@@ -4,14 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-def read_private_key_file():
-    with open("/app/private_key.pem", "r") as f:
-        return f.read()
-
 class Setting:
     STARK_PROJECT_ID: str = os.getenv("STARK_PROJECT_ID")
-    STARK_PRIVATE_KEY_VALUE: str = read_private_key_file()
+    STARK_PRIVATE_KEY_VALUE: str = os.getenv("STARK_PRIVATE_KEY_VALUE")
     ENVIRONMENT = os.getenv("STARK_ENVIRONMENT", "sandbox")
     STARK_WEBHOOK_URL = os.getenv("STARK_WEBHOOK_URL", "https://api.localhost:8000/v2/webhook")
 
